3a5b5469dc1b576e8f83c4b13eb9bfa60b77c094
Jajwol Bajracharya <jajwol.bajracharya@mq.edu.au>
2024-12-11 20:26:34 +1100

    Initial Commit

---
+++ CheckTests/CheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  CheckTests.swift
+//  CheckTests
+//
+//  Created by Jajwol Bajracharya on 11/12/2024.
+//
+
+import Testing
+@testable import Check
+
+struct CheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
